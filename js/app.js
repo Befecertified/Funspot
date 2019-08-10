8f4cac931faa6b3764619db95d4ed9ad60b8574b
befecertified <deekor.baribefe@gmail.com>
2019-08-10 12:10:48 +0100

    Custom Js file.

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let showBtn = document.querySelector('.show-button');
+let videoCard = document.querySelectorAll('.card');
+
+// videoCard.style.display = 'none';
+showBtn.addEventListener('click', ()=> {
+    videoCard.style.display = 'none';
+})
